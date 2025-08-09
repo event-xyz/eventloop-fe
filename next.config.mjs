79071d355f18efd9ee59d6e44bf4415a87e5edb8
lalithbseervi <lcoding20@gmail.com>
2025-08-09 18:14:37 +0530

    fix: downgrade react-qr-scanner to be compatible with react

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        'fs/promises': false,
-        tls: false,  // Add this
-        child_process: false,  // Add this
-        readline: false,  // Add this
-        module: false,  // Add this
-      };
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
